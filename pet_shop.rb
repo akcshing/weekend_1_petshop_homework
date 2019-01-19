f0838b6875a9c87a6a5b9d2dd74be2733fce23cf
Alexkcshing <alexkcshing@gmail.com>
2019-01-19 11:21:56 +0000

    add explicit return values

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,9 +1,11 @@
 def pet_shop_name(pet_shop)
   shop_name = pet_shop[:name]
+  return shop_name
 end
 
 def total_cash(pet_shop)
-  return pet_shop[:admin][:total_cash]
+  total_cash = pet_shop[:admin][:total_cash]
+  return total_cash
 end
 
 def add_or_remove_cash(pet_shop, cash)
@@ -12,6 +14,14 @@ end
 
 def pets_sold(pets_sold)
   pets_sold = pets_sold[:admin][:pets_sold]
+  return pets_sold
 end
 
-def increase_pets_sold(pet)
+def increase_pets_sold(pets_sold_total, pets_sold)
+  pets_sold_total[:admin][:pets_sold] += pets_sold
+end
+
+def stock_count(pet_shop)
+  pet_count = pet_shop[:pets].count
+  return pet_count
+end
